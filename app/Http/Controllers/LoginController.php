e10a73003f513a09bd3ff80b22dcb520f35fbf1f
Swordigo15 <annys.abubakar@gmail.com>
2024-01-19 08:16:38 +0700

    Login/Logout & Middleware

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -12,5 +13,26 @@ class LoginController extends Controller
             'title' => 'Login',
             'active' => 'login'
         ]);
+    }
+
+    public function authenticate(Request $request){
+        $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('loginError', 'Login failed!');
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/Home');
     }
 }
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -9,32 +9,51 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link {{ ($active === "home") ? 'active' : '' }}" href="/Home">Home</a>
+                    <a class="nav-link {{ $active === 'home' ? 'active' : '' }}" href="/Home">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($active === "about") ? 'active' : '' }}" href="/About">About</a>
+                    <a class="nav-link {{ $active === 'about' ? 'active' : '' }}" href="/About">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($active === "blog") ? 'active' : '' }}" href="/Post">Blog</a>
+                    <a class="nav-link {{ $active === 'blog' ? 'active' : '' }}" href="/Post">Blog</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($active === "category") ? 'active' : '' }}" href="/Categories">Category</a>
+                    <a class="nav-link {{ $active === 'category' ? 'active' : '' }}" href="/Categories">Category</a>
                 </li>
             </ul>
 
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}">
-                        <i class="bi bi-box-arrow-in-right"></i>
-                        Login
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/register" class="nav-link {{ ($active === "register") ? 'active' : '' }}">
-                        Sign Up
-                    </a>
-                </li>
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Welcome back, {{ auth()->user()->name }}
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li>
+                                <form action="/logout" method="post">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</a></button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link {{ $active === 'login' ? 'active' : '' }}">
+                            <i class="bi bi-box-arrow-in-right"></i>
+                            Login
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/register" class="nav-link {{ $active === 'register' ? 'active' : '' }}">
+                            Sign Up
+                        </a>
+                    </li>
+                @endauth
             </ul>
         </div>
     </div>
-</nav> {{-->>> Navbar End --}}
+</nav> {{-- >>> Navbar End --}}
